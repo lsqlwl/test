888bd747a42cfe963aa20c844ac2f12716dd1d94
lsqlwl <82612943@qq.com>
2016-08-30 20:49:36 +0800

    'ddddssss'

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+ 
+gulp.task('minify-css', function() {
+  return gulp.src('styles/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist'));
+});
